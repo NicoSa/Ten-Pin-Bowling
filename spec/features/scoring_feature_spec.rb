651826dd059006415af5fddda45a38746adf1522
Nico Saueressig <nico@nicosaueressig.de>
2014-07-01 18:58:36 +0100

    deleted launchy files, added gitignore

--- spec/features/scoring_feature_spec.rb
+++ spec/features/scoring_feature_spec.rb
@@ -8,9 +8,9 @@ describe 'Scoring>' do
 			visit ('/')
 			expect(page).to_not have_content('6')
 			within('.frame_one') do
-				fill_in('roll_one', with: 3)
-				fill_in('roll_two', with: 5)
-				click_on('Next')
+				page.fill_in('#roll_one', with: '3')
+				page.fill_in('#roll_two', with: '5')
+				page.click_on('Next')
 			end
 			expect(page).to have_content('6')
 		end
@@ -18,7 +18,7 @@ describe 'Scoring>' do
 		it 'throws an error when wrong format is entered and doesn´t change total score' do
 			visit('/')
 			within('.frame_one') do
-				fill_in('roll_one', with: 15)
+				fill_in('#roll_one', with: '15')
 				click_on('Next')
 			end
 			expect(page).to have_content('You can´t knock down more than ten pins in a roll')
@@ -29,8 +29,8 @@ describe 'Scoring>' do
 		it 'displays an / if frame was a spare' do
 			visit('/')
 			within('.frame_one') do
-				fill_in('roll_one', with: 5)
-				fill_in('roll_two', with: 5)
+				fill_in('#roll_one', with: '5')
+				fill_in('#roll_two', with: '5')
 				click_on('Next')
 			end
 			expect(page).to_not have_content('You can´t knock down more than ten pins in a roll')
@@ -41,7 +41,7 @@ describe 'Scoring>' do
 		it 'only displays an X and an empty field when your first roll is a strike' do
 			visit('/')
 			within('.frame_one') do
-				fill_in('roll_one', with: 10)
+				fill_in('roll_one', with: '10')
 				click_on('Next')
 			end
 			expect(page).to_not have_content('You can´t knock down more than ten pins in a roll')
@@ -55,15 +55,17 @@ describe 'Scoring>' do
 		it 'displays 15 if first frame was a spare and roll one in second frame was 5' do
 			visit('/')
 			within('.frame_one') do
-				fill_in('roll_one', with: 5)
-				fill_in('roll_two', with: 5)
+				fill_in('roll_one', with: '5')
+				puts "filled in worked"
+				fill_in('roll_two', with: '5')
+				puts "filled in works"
 				click_on('Next')
 			end
 			expect(page).to_not have_content('You can´t knock down more than ten pins in a roll')
 			expect(page).to have_content('10')
 			expect(page).to have_content('/')
 			within('.frame_two') do
-				fill_in('roll_one', with:  5)
+				fill_in('roll_one', with:  '5')
 				click_on('Next')
 			end
 			expect(page).to_not have_content('You can´t knock down more than ten pins in a roll')
